f5a7ce2db5de209ebe8446493698ef766806c3ec
Denys Popov <denys@denyspopov.biz>
2011-12-04 18:04:31 +0200

    Initial game window.

---
+++ ImpressiveSolids/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace ImpressiveSolids {
+    class Game : GameWindow {
+        [STAThread]
+        static void Main() {
+            using (var Game = new Game()) {
+                Game.Run(30);
+            }
+        }
+
+        public Game()
+            : base(700, 500, GraphicsMode.Default, "Impressive Solids") {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs E) {
+            base.OnLoad(E);
+        }
+
+        protected override void OnResize(EventArgs E) {
+            base.OnResize(E);
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs E) {
+            base.OnUpdateFrame(E);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs E) {
+            base.OnRenderFrame(E);
+
+            GL.ClearColor(Color4.Black);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
+    }
+}
