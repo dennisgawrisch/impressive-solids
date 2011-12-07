651c335a9c9b0bf8493db04dca1744c6277eb88b
Denys Popov <besisland@besisland.name>
2011-12-08 01:17:36 +0200

    Deployment package, icon, assembly/product info.

---
+++ ImpressiveSolids/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Impressive Solids")]
+[assembly: AssemblyDescription("A game of falling blocks")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Denys Popov")]
+[assembly: AssemblyProduct("Impressive Solids")]
+[assembly: AssemblyCopyright("GPLv3 license")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b7dcfc1-a261-48be-89cd-fb1b81a97f78")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
