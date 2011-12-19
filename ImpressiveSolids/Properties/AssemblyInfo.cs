4d6bde63751a330fbff2ff569555e171d7af095f
Denys Popov <besisland@besisland.name>
2011-12-19 22:15:13 +0200

    Version 3 ? 4 in metafiles.

--- ImpressiveSolids/Properties/AssemblyInfo.cs
+++ ImpressiveSolids/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
